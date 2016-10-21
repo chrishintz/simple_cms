538a85ce6d29c1686c39d52a5bd40e86d0330c03
Chris Hintz <chrishintz42@gmail.com>
2016-10-21 15:49:01 -0700

    building out templates

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  get 'demo/index'
+  root 'demo#index'
 
+  get 'demo/index'
+  get 'demo/hello'
+  get 'demo/other_hello'
+  get 'demo/lynda'
+  # get ':controller(/:action(/:id))'
 end
