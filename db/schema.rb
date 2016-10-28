2d0a46997a2f8e0616f7ac1644c416469d01fd67
Chris Hintz <chrishintz42@gmail.com>
2016-10-27 18:13:39 -0700

    Add restricted Access

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161023205524) do
+ActiveRecord::Schema.define(version: 20161027232454) do
 
   create_table "admin_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "first_name",      limit: 25
